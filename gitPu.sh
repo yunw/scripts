037ba51f535d08895e32d2998e7e2e2f6eb62c1a
Irene Yun Wang <yunwang@tibco.com>
2016-06-10 18:35:15 -0700

    rename

---
+++ gitPu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# first time pull
+
+#git init
+#git remote add origin https://github.com/yunw/scripts.git
+
+# show remote repository
+git remote -v
+
+# git push [remote-name] [branch-name]
+git push origin master
+
+
